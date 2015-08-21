5930d9de8772037289b492fdbc58cc39ed63951d
Rose <rosexu1996@yahoo.com>
2015-08-20 23:28:39 -0400

    add light to scene

--- js/threejshome.js
+++ js/threejshome.js
@@ -1,16 +1,21 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var light = new THREE.PointLight( 0xff0000, 1, 100 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshLambertMaterial( { color: "#990000", 
+												shading: THREE.SmoothShading } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+
+light.position.set( 3, 3, 3 );
+scene.add( light );
 
 var render = function () {
 	requestAnimationFrame( render );
